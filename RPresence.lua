1e957aefb1f779292c0cfb4a778bc02297beaba2
6ixfalls <bryan888red@gmail.com>
2021-10-07 21:17:11 -0700

    Add pcalls to HTTP requests

--- RPresence.lua
+++ RPresence.lua
@@ -2,10 +2,12 @@ local HTTPService = game:GetService("HttpService")
 
 function UpdateData()
 	coroutine.resume(coroutine.create(function()
-		HTTPService:PostAsync("http://localhost:2043/update", HTTPService:JSONEncode({
-			name = game.Name,
-			gameId = game.PlaceId
-		}))
+		pcall(function()
+			HTTPService:PostAsync("http://localhost:2043/update", HTTPService:JSONEncode({
+				name = game.Name,
+				gameId = game.PlaceId
+			}))
+		end)
 	end))
 end
 
@@ -14,5 +16,7 @@ UpdateData()
 local Listener = game:GetPropertyChangedSignal("Name"):Connect(UpdateData)
 
 plugin.Unloading:Connect(function()
-	HTTPService:GetAsync("http://localhost:2043/clear")
+	pcall(function()
+		HTTPService:GetAsync("http://localhost:2043/clear")
+	end)
 end)
